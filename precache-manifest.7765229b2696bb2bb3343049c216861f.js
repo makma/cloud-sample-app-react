7119364031ba8ed6ffedb48ddb4ce9fcd4dc693a
MASTER\martinh <martinh@kentico.com>
2019-05-03 10:07:21 +0200

    Updates

--- precache-manifest.7765229b2696bb2bb3343049c216861f.js
+++ precache-manifest.7765229b2696bb2bb3343049c216861f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = [
   {
-    "revision": "392cb2e0295e00036c634860a8f0cbad",
-    "url": "/cloud-sample-app-react/static/media/checkbox-checked.392cb2e0.svg"
+    "revision": "1a84a9c1102aa390f7d788a9a4f4603c",
+    "url": "/cloud-sample-app-react/static/media/radio-button-checked.1a84a9c1.svg"
   },
   {
-    "revision": "9c19ec3bf1ddb3d91d7b",
+    "revision": "21b20fe120b935ac9d76",
     "url": "/cloud-sample-app-react/static/css/main.104d89e2.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = [
     "url": "/cloud-sample-app-react/static/js/runtime~main.b4e3ef04.js"
   },
   {
-    "revision": "3ecb1cb78ad5183a1be0",
-    "url": "/cloud-sample-app-react/static/js/2.3ecb1cb7.chunk.js"
+    "revision": "a18232edaf26397b338b",
+    "url": "/cloud-sample-app-react/static/js/2.a18232ed.chunk.js"
   },
   {
     "revision": "171ce4e4cb28e074f5c9967f7db949b3",
@@ -32,51 +32,51 @@ self.__precacheManifest = [
     "url": "/cloud-sample-app-react/static/media/checkbox-unchecked.abf2d2b0.svg"
   },
   {
-    "revision": "1a84a9c1102aa390f7d788a9a4f4603c",
-    "url": "/cloud-sample-app-react/static/media/radio-button-checked.1a84a9c1.svg"
-  },
-  {
     "revision": "8c6533e10367d0901e362f7d295e03b9",
     "url": "/cloud-sample-app-react/static/media/radio-button.8c6533e1.svg"
   },
   {
-    "revision": "9c19ec3bf1ddb3d91d7b",
-    "url": "/cloud-sample-app-react/static/js/main.9c19ec3b.chunk.js"
+    "revision": "392cb2e0295e00036c634860a8f0cbad",
+    "url": "/cloud-sample-app-react/static/media/checkbox-checked.392cb2e0.svg"
+  },
+  {
+    "revision": "21b20fe120b935ac9d76",
+    "url": "/cloud-sample-app-react/static/js/main.21b20fe1.chunk.js"
   },
   {
     "revision": "ccdb76a033026dbdc3f08c50f4a5263f",
     "url": "/cloud-sample-app-react/static/media/SourceSansPro-Light.ccdb76a0.woff"
   },
   {
-    "revision": "fdc0354668675e460e18d8e6b880e634",
-    "url": "/cloud-sample-app-react/static/media/SourceSansPro-Regular.fdc03546.woff"
-  },
-  {
     "revision": "54f926c7e2cffb824547c75bc5ac0b5e",
     "url": "/cloud-sample-app-react/static/media/SourceSansPro-Bold.54f926c7.woff"
+  },
+  {
+    "revision": "fdc0354668675e460e18d8e6b880e634",
+    "url": "/cloud-sample-app-react/static/media/SourceSansPro-Regular.fdc03546.woff"
   },
   {
     "revision": "b2622e38f246209fb933e8b8a6ee76ed",
     "url": "/cloud-sample-app-react/static/media/PTSerif-Bold.b2622e38.woff"
   },
   {
-    "revision": "4611bc4557599035d912ff07366ad002",
-    "url": "/cloud-sample-app-react/static/media/Core-icons.4611bc45.eot"
+    "revision": "14fb01c406a39aab06275f5578c7316f",
+    "url": "/cloud-sample-app-react/static/media/Core-icons.14fb01c4.woff"
   },
   {
     "revision": "1b9af651cbf6b3f133faf2bede0e5266",
     "url": "/cloud-sample-app-react/static/media/Core-icons.1b9af651.ttf"
   },
   {
-    "revision": "14fb01c406a39aab06275f5578c7316f",
-    "url": "/cloud-sample-app-react/static/media/Core-icons.14fb01c4.woff"
+    "revision": "4611bc4557599035d912ff07366ad002",
+    "url": "/cloud-sample-app-react/static/media/Core-icons.4611bc45.eot"
   },
   {
     "revision": "6823218d348f1ba4e035908b672c745a",
     "url": "/cloud-sample-app-react/static/media/Core-icons.6823218d.svg"
   },
   {
-    "revision": "e7f0f66f0af54560bd67a000a4e80c4c",
+    "revision": "779e7d6bd770802aaa1e71ccd1aab5cc",
     "url": "/cloud-sample-app-react/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cloud-sample-app-react/precache-manifest.5857c4398be3d95b5e0e0e08ef2310b7.js"
+  "/cloud-sample-app-react/precache-manifest.7765229b2696bb2bb3343049c216861f.js"
 );
 
 workbox.clientsClaim();
